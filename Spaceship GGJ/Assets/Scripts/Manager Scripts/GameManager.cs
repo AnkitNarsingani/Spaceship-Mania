4978814a43697350e11de4033cd8d50c27caaf29
Ankit <43947543+AnkitNarsingani@users.noreply.github.com>
2020-02-01 12:47:04 +0530

    Added Managers

---
+++ Spaceship GGJ/Assets/Scripts/Manager Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    float health;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public float getCurrentHealth()
+    {
+        return health;
+    }
+
+    public void takeDamage(float DamageAmount)
+    {
+        health -= DamageAmount;
+    }
+
+    public void heal(float HealAmount)
+    {
+        health += HealAmount;
+    }
+}
